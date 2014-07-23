cd998627da460a53a4f37c8291ae8bd5d95ba45e
Christopher Su <christophersu9@gmail.com>
2014-07-23 15:34:32 -0700

    server

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, request
+import json
+from jira.client import JIRA
 from configuration import *
+import urllib2
 
 app = Flask(__name__)
+
+def auth_jira_url(url):
+    return url + "&os_username=" + JIRA_USERNAME + "&os_password=" + JIRA_PASSWORD
 
 ####################################################################
 # Routes
@@ -10,9 +16,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', header='Flask Skeleton', title='Flask Skeleton', body='''
-        <p>Some content can go here!</p>
-        ''')
+    return app.send_static_file('index.html')
+
+@app.route('/jira/do_today', methods=['GET'])
+def jira_do_today_count():
+    return urllib2.urlopen(auth_jira_url(JIRA_BASE_URL + "/rest/api/2/search?jql=status=\"Do%20Today\"")).read()
 
 ####################################################################
 # Start Flask
